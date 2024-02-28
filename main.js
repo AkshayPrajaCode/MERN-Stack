6312908affe806f561621c121e72a744bce9ad96
AkshayPrajaCode <akshaykumarprajapati@hotmail.com>
2024-02-27 23:10:21 -0800

    function

--- main.js
+++ main.js
@@ -1,24 +1,8 @@
-var user={
-    Name :"Akshay Kumar Prajapati",
-    email:"akshaykumarprajapati@hotmail.com",
-    Mobile : "8987918309",
-    Address: {
-        StreetName : "Vijaya Nagar",
-        City : "Bhilwara",
-        State : "Rajasthan",
-        Country : "India"
-    }
-}
-console.log(user);
-//Accessing the property of an object using dot notation  
-console.log("User's name is "+user.Name)  
+console.log("Node js");
 
-//Accessing nested objects in JavaScript 
-console.log("City where User lives   is "+user.Address.City)            
 
-var person = function (a,b){
-    var c= a+b;
-    console.log("Addition of a and b   "+c)
-}
+let sum =function(){
+    console.log("Function");
+    };
 
-person(4,5);
+    sum();
