1622f1e60ee78d4f3149bb19857e56452cb54ac1
AkshayPrajaCode <akshaykumarprajapati@hotmail.com>
2024-02-27 23:27:08 -0800

    login

---
+++ login.js
@@ -0,0 +1,6 @@
+let email = "akshaykumarprajapati@hotmail.com";
+password= "123456";
+
+if(email=="akshaykumarprajapati@hotmail.com" && password=="123456"){
+    console.log("Wel come to the dashboard");
+}
--- main.js
+++ main.js
@@ -6,3 +6,20 @@ let sum =function(){
     };
 
     sum();
+
+
+
+    const person={
+        Name :"Akshay Kumar Prajapti",
+        Email:"akshaykumarprajapati@hotmail.com"
+  };
+  console.log(person);
+
+
+
+
+  const login = {
+    Email:"akshaykumarprajapati@hotmail.com",
+    password:"localhost@123"
+  }
+  console.log( "login id and password ",{login});
